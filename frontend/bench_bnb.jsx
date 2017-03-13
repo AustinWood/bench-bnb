70164f4c5e89620fc507061479775d9a17d148f0
Austin Wood <austin@austinkwood.com>
2017-03-13 16:53:30 -0700

    Logout via UI

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
+import Root from './components/root';
 
-import { login, signup, logout } from './util/session_api_util.js';
+import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
-
-    window.login = login;
-    window.signup = signup;
-    window.logout = logout;
-    window.store = configureStore();
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
+  window.store = store;
+  window.login = login;
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,27 +1,30 @@
-import { RECEIVE_CURRENT_USER,
-         RECEIVE_LOGOUT_SUCCESS,
-         RECEIVE_ERRORS }
-         from '../actions/session_actions';
-
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT,
+  RECEIVE_ERRORS } from '../actions/session_actions';
 import merge from 'lodash/merge';
 
-const _nullUser = {
+const _nullUser = Object.freeze({
   currentUser: null,
   errors: []
-};
+});
 
 const SessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  console.log(action);
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return {
-        currentUser: action.currentUser,
-        errors: []
-      };
-    case RECEIVE_LOGOUT_SUCCESS:
+      const currentUser = action.currentUser;
+      return merge({}, _nullUser, {
+        currentUser
+      });
+    case LOGOUT:
       return merge({}, _nullUser);
     case RECEIVE_ERRORS:
-      const errors = {errors: action.errors};
-      return merge({}, state, errors);
+      const errors = action.errors;
+      return merge({}, _nullUser, {
+        errors
+      });
     default:
       return state;
   }
