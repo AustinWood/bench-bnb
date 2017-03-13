e3932cc64f2aa423c7d21f30e15e1b4b832b1773
Austin Wood <austin@austinkwood.com>
2017-03-13 14:48:57 -0700

    Store saves to window and begins with initial state of null session

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
 
 import { login, signup, logout } from './util/session_api_util.js';
 
@@ -10,4 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.login = login;
     window.signup = signup;
     window.logout = logout;
+    window.store = configureStore();
 });
