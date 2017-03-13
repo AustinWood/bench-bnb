919b2689285ac6f642fcc6bf5b02f024e47a6825
Austin Wood <austin@austinkwood.com>
2017-03-13 12:02:28 -0700

    Front end auth working through console

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,24 +1,24 @@
 import { receiveCurrentUser, receiveErrors } from '../actions/session_actions';
 
-export const login = (user) => {
-  return $.ajax({
-    method: 'POST',
-    url: '/api/session',
-    data: user
-  });
-};
-
 export const signup = (user) => {
   return $.ajax({
     method: 'POST',
-    url: '/api/user',
-    data: user
+    url: './api/users',
+    data: {user}
+  });
+};
+
+export const login = (user) => {
+  return $.ajax({
+    method: 'POST',
+    url: './api/session',
+    data: {user}
   });
 };
 
 export const logout = () => {
   return $.ajax({
     method: 'delete',
-    url: '/api/session'
+    url: './api/session'
   });
 };
